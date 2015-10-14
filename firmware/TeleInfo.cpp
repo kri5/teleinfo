b825682d0a2a6b3aef3d761c5adcb966f4e6fa45
Christophe Courtaut <christophe.courtaut@gmail.com>
2015-10-14 12:33:43 +0200

    Fixes indentation

--- firmware/TeleInfo.cpp
+++ firmware/TeleInfo.cpp
@@ -1,10 +1,10 @@
 /*
-TeleInfo - une bibliothèque de décodage des trames du compteur électrique pour Spark Core
-Inspiré de la philo de la bibliothèque TinyGPS de Mikal Hart
-D'après le document ERDF-NOI-CPT_02E - Version 2 - 01/03/2008
-Copyright (C) 2014 Thibault Ducret
-Licence MIT
-*/
+   TeleInfo - une bibliothèque de décodage des trames du compteur électrique pour Spark Core
+   Inspiré de la philo de la bibliothèque TinyGPS de Mikal Hart
+   D'après le document ERDF-NOI-CPT_02E - Version 2 - 01/03/2008
+   Copyright (C) 2014 Thibault Ducret
+   Licence MIT
+   */
 
 #include "TeleInfo.h"
 
@@ -23,13 +23,13 @@ Licence MIT
 #define _LABEL_STATE_WORD               "MOTDETAT"
 
 
-TeleInfo::TeleInfo()
+  TeleInfo::TeleInfo()
   :  subscribed_intensity(INVALID_UINT)
   ,  off_peak_hours_index(INVALID_UINT)
   ,  peak_hours_index(INVALID_UINT)
   ,  instant_intensity(INVALID_UINT)
   ,  max_intensity(INVALID_UINT)
-  ,  appearing_power(INVALID_UINT)
+     ,  appearing_power(INVALID_UINT)
 {
   meter_address[0] = 0;
   rate_option[0] = 0;
@@ -45,89 +45,89 @@ TeleInfo::TeleInfo()
 
 bool TeleInfo::handleError()
 {
-	this->currentState = &TeleInfo::waitForFrameBegining;
-	return true;
+  this->currentState = &TeleInfo::waitForFrameBegining;
+  return true;
 }
 
 bool TeleInfo::waitForFrameBegining(char c)
 {
-	if (c == 0x02)
-	{
-		this->currentState = &TeleInfo::waitForGroupStart;
-		return true;
-	}
-	return false;
+  if (c == 0x02)
+  {
+    this->currentState = &TeleInfo::waitForGroupStart;
+    return true;
+  }
+  return false;
 }
 
 bool TeleInfo::waitForGroupStart(char c)
 {
-	if (c == 0x0A)
-	{
-		this->group_numbers = 0;
-		memset(this->groups, 0, sizeof(this->groups)); // Reset the array
-		this->currentState = &TeleInfo::createLabel;
-	}
-	return true;
+  if (c == 0x0A)
+  {
+    this->group_numbers = 0;
+    memset(this->groups, 0, sizeof(this->groups)); // Reset the array
+    this->currentState = &TeleInfo::createLabel;
+  }
+  return true;
 }
 
 bool TeleInfo::createLabel(char c)
 {
-	if (_nbCarEtat2 == 0)
-	{
-		memset(label, 0, sizeof(label)); // Reset the label
-	}
-	if (c == 0x20) // end of the label
-	{
-		_nbCarEtat2 = 0;
-		this->currentState = &TeleInfo::createData;
-	}
-	else if (_nbCarEtat2 < LABEL_MAX_SIZE)
-	{
-		label[_nbCarEtat2] = c;
-		_nbCarEtat2 += 1;
-	}
-	else
-	{
-		_nbCarEtat2 = 0;
-		return false;
-	}
-	return true;
+  if (_nbCarEtat2 == 0)
+  {
+    memset(label, 0, sizeof(label)); // Reset the label
+  }
+  if (c == 0x20) // end of the label
+  {
+    _nbCarEtat2 = 0;
+    this->currentState = &TeleInfo::createData;
+  }
+  else if (_nbCarEtat2 < LABEL_MAX_SIZE)
+  {
+    label[_nbCarEtat2] = c;
+    _nbCarEtat2 += 1;
+  }
+  else
+  {
+    _nbCarEtat2 = 0;
+    return false;
+  }
+  return true;
 }
 
 bool TeleInfo::createData(char c)
 {
-	if (_nbCarEtat3 == 0)
-	{
-		memset(this->data, 0, sizeof(this->data)); // reset data
-	}
-	if (c == 0x20) // end of data
-	{
-		_nbCarEtat3 = 0;
-		this->currentState = &TeleInfo::verifyChecksum;
-	}
-	else if (_nbCarEtat3 < DATA_MAX_SIZE)
-	{
-		data[_nbCarEtat3] = c;
-		_nbCarEtat3 += 1;
-	}
-	else
-	{
-		_nbCarEtat3 = 0;
-		return false;
-	}
-	return true;
+  if (_nbCarEtat3 == 0)
+  {
+    memset(this->data, 0, sizeof(this->data)); // reset data
+  }
+  if (c == 0x20) // end of data
+  {
+    _nbCarEtat3 = 0;
+    this->currentState = &TeleInfo::verifyChecksum;
+  }
+  else if (_nbCarEtat3 < DATA_MAX_SIZE)
+  {
+    data[_nbCarEtat3] = c;
+    _nbCarEtat3 += 1;
+  }
+  else
+  {
+    _nbCarEtat3 = 0;
+    return false;
+  }
+  return true;
 }
 
 bool TeleInfo::verifyChecksum(char c)
 {
-	if (_nbCarEtat4 == 0)
-	{
-		_checksum = c;
-		_nbCarEtat4 += 1;
-	}
-	else if (c == 0x0D) // end of checksum
-	{
-		_nbCarEtat4 = 0;
+  if (_nbCarEtat4 == 0)
+  {
+    _checksum = c;
+    _nbCarEtat4 += 1;
+  }
+  else if (c == 0x0D) // end of checksum
+  {
+    _nbCarEtat4 = 0;
     int i = 0;
     unsigned char sum = 0x20;
     for (i = 0; label[i] != 0; i++)
@@ -140,44 +140,44 @@ bool TeleInfo::verifyChecksum(char c)
       _nbCarEtat4 = 0;
       this->currentState = &TeleInfo::waitForGroupOrFrameEnd;
     }
-		if (group_numbers >= NB_GROUP_MAX)
-		{
-			return false;
-		}
-		else
-		{
-			memcpy(groups[group_numbers].label, label, sizeof(label));
-			memcpy(groups[group_numbers].data, data, sizeof(data));
-			group_numbers += 1;
+    if (group_numbers >= NB_GROUP_MAX)
+    {
+      return false;
+    }
+    else
+    {
+      memcpy(groups[group_numbers].label, label, sizeof(label));
+      memcpy(groups[group_numbers].data, data, sizeof(data));
+      group_numbers += 1;
       _nbCarEtat4 = 0;
-			this->currentState = &TeleInfo::waitForGroupOrFrameEnd;
-		}
-	}
-	else
-	{
-		_nbCarEtat4 = 0;
-		return false;
-	}
-	return true;
+      this->currentState = &TeleInfo::waitForGroupOrFrameEnd;
+    }
+  }
+  else
+  {
+    _nbCarEtat4 = 0;
+    return false;
+  }
+  return true;
 }
 
 bool TeleInfo::waitForGroupOrFrameEnd(char c)
 {
-	if (c == 0x0A) // start of a new group
-	{
-		this->currentState = &TeleInfo::createLabel;
-	}
-	else if (c == 0x03) // end of frame
-	{ // valid frame
-		is_frame_valid = true;
-		setVariables();
-		this->currentState = &TeleInfo::waitForFrameBegining;
-	}
-	else
-	{
-		return false;
-	}
-	return true;
+  if (c == 0x0A) // start of a new group
+  {
+    this->currentState = &TeleInfo::createLabel;
+  }
+  else if (c == 0x03) // end of frame
+  { // valid frame
+    is_frame_valid = true;
+    setVariables();
+    this->currentState = &TeleInfo::waitForFrameBegining;
+  }
+  else
+  {
+    return false;
+  }
+  return true;
 }
 
 bool TeleInfo::decode(char c)
@@ -185,7 +185,7 @@ bool TeleInfo::decode(char c)
   this->is_frame_valid = false;
   if (!(this->*currentState)(c))
   {
-	  this->handleError();
+    this->handleError();
   }
 
   return this->is_frame_valid;
@@ -197,25 +197,25 @@ void TeleInfo::setVariables()
 
   for (i=0; i < group_numbers; i+=1)
   {
-	if (strcmp(groups[i].label,_LABEL_METER_ADDRESS) == 0)
-	{
-  	    memcpy(meter_address,groups[i].data,sizeof(meter_address));
-  	}
-  	else if (strcmp(groups[i].label,_LABEL_APPEARING_POWER) == 0)
-  	{
-  		appearing_power=(unsigned int) atoi(groups[i].data);
-  	}
-  	else if (strcmp(groups[i].label,_LABEL_INSTANT_INTENSITY) == 0)
-  	{
-  		instant_intensity=(unsigned int) atoi(groups[i].data);
-  	}
-  	else if (strcmp(groups[i].label,_LABEL_OFF_PEAK_HOURS) == 0)
-  	{
-  		off_peak_hours_index=(unsigned int) atoi(groups[i].data);
-  	}
-  	else if (strcmp(groups[i].label,_LABEL_PEAK_HOURS) == 0)
-  	{
-  		peak_hours_index=(unsigned int) atoi(groups[i].data);
-  	}
+    if (strcmp(groups[i].label,_LABEL_METER_ADDRESS) == 0)
+    {
+      memcpy(meter_address,groups[i].data,sizeof(meter_address));
+    }
+    else if (strcmp(groups[i].label,_LABEL_APPEARING_POWER) == 0)
+    {
+      appearing_power=(unsigned int) atoi(groups[i].data);
+    }
+    else if (strcmp(groups[i].label,_LABEL_INSTANT_INTENSITY) == 0)
+    {
+      instant_intensity=(unsigned int) atoi(groups[i].data);
+    }
+    else if (strcmp(groups[i].label,_LABEL_OFF_PEAK_HOURS) == 0)
+    {
+      off_peak_hours_index=(unsigned int) atoi(groups[i].data);
+    }
+    else if (strcmp(groups[i].label,_LABEL_PEAK_HOURS) == 0)
+    {
+      peak_hours_index=(unsigned int) atoi(groups[i].data);
+    }
   }
 }
--- firmware/TeleInfo.h
+++ firmware/TeleInfo.h
@@ -1,10 +1,10 @@
 /*
-TeleInfo - une bibliothèque de décodage des trames du compteur électrique pour Spark Core
-Inspiré de la philo de la bibliothèque TinyGPS de Mikal Hart
-D'après le document ERDF-NOI-CPT_02E - Version 2 - 01/03/2008
-Copyright (C) 2014 Thibault Ducret
-Licence MIT
-*/
+   TeleInfo - une bibliothèque de décodage des trames du compteur électrique pour Spark Core
+   Inspiré de la philo de la bibliothèque TinyGPS de Mikal Hart
+   D'après le document ERDF-NOI-CPT_02E - Version 2 - 01/03/2008
+   Copyright (C) 2014 Thibault Ducret
+   Licence MIT
+   */
 #ifndef TeleInfo_h
 #define TeleInfo_h
 
@@ -18,80 +18,80 @@ Licence MIT
 #define INVALID_UINT 65535
 
 typedef struct {
-    char label[LABEL_MAX_SIZE + 1];
-    char data[DATA_MAX_SIZE+1];
+  char label[LABEL_MAX_SIZE + 1];
+  char data[DATA_MAX_SIZE+1];
 } TeleInfoGroup;
 
 class TeleInfo
 {
 
-public:
-  TeleInfo();
-  bool decode(char c);
-  TeleInfo &operator << (char c)
-  {
-	  decode(c); return *this;
-  }
+  public:
+    TeleInfo();
+    bool decode(char c);
+    TeleInfo &operator << (char c)
+    {
+      decode(c); return *this;
+    }
 
-  inline char* meterAddress() { return meter_address; }
-  inline char* rateOption() { return rate_option; }
-  inline unsigned int subscribedIntensity() { return subscribed_intensity; }
-  inline unsigned int offPeakHoursIndex() { return off_peak_hours_index; }
-  inline unsigned int peakHoursIndex() { return peak_hours_index; }
-  inline char *ratePeriod() { return rate_period; }
-  inline unsigned int instantIntensity() { return instant_intensity; }
-  inline char *overuseWarning() { return overuse_warning; }
-  inline unsigned int maxIntensity() { return max_intensity; }
-  inline unsigned int appearingPower() { return appearing_power; }
-  inline char *timeGroup() { return time_group; }
-  inline char *stateWord() { return state_word; }
+    inline char* meterAddress() { return meter_address; }
+    inline char* rateOption() { return rate_option; }
+    inline unsigned int subscribedIntensity() { return subscribed_intensity; }
+    inline unsigned int offPeakHoursIndex() { return off_peak_hours_index; }
+    inline unsigned int peakHoursIndex() { return peak_hours_index; }
+    inline char *ratePeriod() { return rate_period; }
+    inline unsigned int instantIntensity() { return instant_intensity; }
+    inline char *overuseWarning() { return overuse_warning; }
+    inline unsigned int maxIntensity() { return max_intensity; }
+    inline unsigned int appearingPower() { return appearing_power; }
+    inline char *timeGroup() { return time_group; }
+    inline char *stateWord() { return state_word; }
 
-private:
-  bool waitForFrameBegining(char c);
-  bool waitForGroupStart(char c);
-  bool createLabel(char c);
-  bool createData(char c);
-  bool verifyChecksum(char c);
-  bool waitForGroupOrFrameEnd(char c);
-  bool handleError();
+  private:
+    bool waitForFrameBegining(char c);
+    bool waitForGroupStart(char c);
+    bool createLabel(char c);
+    bool createData(char c);
+    bool verifyChecksum(char c);
+    bool waitForGroupOrFrameEnd(char c);
+    bool handleError();
 
-  bool (TeleInfo::*currentState)(char c);
+    bool (TeleInfo::*currentState)(char c);
 
-  // propriétés
-  bool is_frame_valid;
-  char meter_address[DATA_MAX_SIZE];
-  char _new_adCompteur[DATA_MAX_SIZE];
-  char rate_option[DATA_MAX_SIZE];
-  char _new_opTarif[DATA_MAX_SIZE];
-  unsigned int subscribed_intensity;
-  unsigned int _new_iSousc;
-  unsigned int off_peak_hours_index;
-  unsigned int _new_indexHC;
-  unsigned int peak_hours_index;
-  unsigned int _new_indexHP;
-  char rate_period[DATA_MAX_SIZE];
-  char _new_perTarif[DATA_MAX_SIZE];
-  unsigned int instant_intensity;
-  unsigned int _new_iInst;
-  char  overuse_warning[DATA_MAX_SIZE];
-  char _new_avertDep[DATA_MAX_SIZE];
-  unsigned int max_intensity;
-  unsigned int _new_iMax;
-  unsigned int appearing_power;
-  unsigned int _new_pApp;
-  char time_group[DATA_MAX_SIZE];
-  char _new_typeHoraireHPHC[DATA_MAX_SIZE];
-  char state_word[DATA_MAX_SIZE];
-  char _new_motEtat[DATA_MAX_SIZE];
+    // propriétés
+    bool is_frame_valid;
+    char meter_address[DATA_MAX_SIZE];
+    char _new_adCompteur[DATA_MAX_SIZE];
+    char rate_option[DATA_MAX_SIZE];
+    char _new_opTarif[DATA_MAX_SIZE];
+    unsigned int subscribed_intensity;
+    unsigned int _new_iSousc;
+    unsigned int off_peak_hours_index;
+    unsigned int _new_indexHC;
+    unsigned int peak_hours_index;
+    unsigned int _new_indexHP;
+    char rate_period[DATA_MAX_SIZE];
+    char _new_perTarif[DATA_MAX_SIZE];
+    unsigned int instant_intensity;
+    unsigned int _new_iInst;
+    char  overuse_warning[DATA_MAX_SIZE];
+    char _new_avertDep[DATA_MAX_SIZE];
+    unsigned int max_intensity;
+    unsigned int _new_iMax;
+    unsigned int appearing_power;
+    unsigned int _new_pApp;
+    char time_group[DATA_MAX_SIZE];
+    char _new_typeHoraireHPHC[DATA_MAX_SIZE];
+    char state_word[DATA_MAX_SIZE];
+    char _new_motEtat[DATA_MAX_SIZE];
 
-  unsigned int _nbCarEtat2, _nbCarEtat3, _nbCarEtat4;
-  char label[LABEL_MAX_SIZE];
-  char data[DATA_MAX_SIZE];
-  char _checksum;
-  TeleInfoGroup groups[NB_GROUP_MAX];
-  unsigned int group_numbers;
+    unsigned int _nbCarEtat2, _nbCarEtat3, _nbCarEtat4;
+    char label[LABEL_MAX_SIZE];
+    char data[DATA_MAX_SIZE];
+    char _checksum;
+    TeleInfoGroup groups[NB_GROUP_MAX];
+    unsigned int group_numbers;
 
-  void setVariables();
+    void setVariables();
 };
 
 #endif
